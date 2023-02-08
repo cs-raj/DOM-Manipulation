99bf9aef44b37b4d7feb3e6e5e36170092fc7bf7
cs-raj <122264749+cs-raj@users.noreply.github.com>
2023-02-08 17:44:58 +0530

    Modified the link for the timer stylesheet

--- src/timer/timer.js
+++ src/timer/timer.js
@@ -17,7 +17,7 @@ var link = document.createElement('link');
 link.rel = 'stylesheet';
      
 link.type = 'text/css';
-link.href = '../style/timer.css';
+link.href = './style/timer.css';
 head.appendChild(link);
 
 
